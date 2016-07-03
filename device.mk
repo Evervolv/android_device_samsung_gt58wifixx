18a13f0a448d010896bbb077c64e6ace1d6e20e3
Sean Hoyt <seanhoyt963@gmail.com>
2016-07-03 00:03:30 -0800

    Remove Handset_cal.acdb
    Only used on LTE devices
    
    Change-Id: Ib48e5f20d94cf2912805b201e5f85d6909ae689f

--- device.mk
+++ device.mk
@@ -28,7 +28,6 @@ PRODUCT_COPY_FILES += \
     device/samsung/gt58wifixx/audio/acdb/Bluetooth_cal.acdb:system/etc/Bluetooth_cal.acdb \
     device/samsung/gt58wifixx/audio/acdb/General_cal.acdb:system/etc/General_cal.acdb \
     device/samsung/gt58wifixx/audio/acdb/Global_cal.acdb:system/etc/Global_cal.acdb \
-    device/samsung/gt58wifixx/audio/acdb/Handset_cal.acdb:system/etc/Handset_cal.acdb \
     device/samsung/gt58wifixx/audio/acdb/Hdmi_cal.acdb:system/etc/Hdmi_cal.acdb \
     device/samsung/gt58wifixx/audio/acdb/Headset_cal.acdb:system/etc/Headset_cal.acdb \
     device/samsung/gt58wifixx/audio/acdb/Speaker_cal.acdb:system/etc/Speaker_cal.acdb \
